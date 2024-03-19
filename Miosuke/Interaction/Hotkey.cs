553979e7b7c7c59880daa8ac3034ca4d242c5c91
Elypha Rin <i@elypha.com>
2024-03-20 04:10:15 +0900

    add hotkey extension

--- Miosuke/Interaction/Hotkey.cs
+++ Miosuke/Interaction/Hotkey.cs
@@ -1,5 +1,4 @@
 using Dalamud.Game.ClientState.Keys;
-using System.Collections.Generic;
 using System.Linq;
 using System;
 
@@ -24,25 +23,4 @@ public class Hotkey
         }
         return true;
     }
-}
-
-public static class Extensions
-{
-    private static readonly Dictionary<VirtualKey, string> NamedKeys = new() {
-        { VirtualKey.KEY_0, "0"},
-        { VirtualKey.KEY_1, "1"},
-        { VirtualKey.KEY_2, "2"},
-        { VirtualKey.KEY_3, "3"},
-        { VirtualKey.KEY_4, "4"},
-        { VirtualKey.KEY_5, "5"},
-        { VirtualKey.KEY_6, "6"},
-        { VirtualKey.KEY_7, "7"},
-        { VirtualKey.KEY_8, "8"},
-        { VirtualKey.KEY_9, "9"},
-        { VirtualKey.CONTROL, "Ctrl"},
-        { VirtualKey.MENU, "Alt"},
-        { VirtualKey.SHIFT, "Shift"},
-    };
-
-    public static string GetKeyName(this VirtualKey k) => NamedKeys.TryGetValue(k, out var value) ? value : k.ToString();
 }
