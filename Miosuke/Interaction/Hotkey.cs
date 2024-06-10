578271365d50b825a9983d0b788a287ed105f168
Elypha Rin <i@elypha.com>
2024-06-10 22:35:51 +0900

    fix hotkey method

--- Miosuke/Interaction/Hotkey.cs
+++ Miosuke/Interaction/Hotkey.cs
@@ -1,6 +1,7 @@
 using Dalamud.Game.ClientState.Keys;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 
 namespace Miosuke;
@@ -8,14 +9,35 @@ namespace Miosuke;
 public class Hotkey
 {
     // -------------------------------- hotkey methods --------------------------------
-    public static bool IsActive(VirtualKey[] keys)
+    public static List<VirtualKey> GetActiveKeys()
     {
+        var activeKeys = new List<VirtualKey>();
+        foreach (var vk in Service.KeyState.GetValidVirtualKeys())
+        {
+            if (Service.KeyState[vk]) activeKeys.Add(vk);
+        }
+        return activeKeys;
+    }
+
+    public static bool IsActive(VirtualKey[] keys, bool strict = false)
+    {
+        // check if all keys are active
         foreach (var vk in keys)
         {
             if (!Service.KeyState.IsVirtualKeyValid(vk)) continue;
 
             if (!Service.KeyState[vk]) return false;
         }
+
+        // if strict, check if all active keys are in the list
+        if (strict)
+        {
+            foreach (var vk in GetActiveKeys())
+            {
+                if (!keys.Contains(vk)) return false;
+            }
+        }
+
         return true;
     }
 }
