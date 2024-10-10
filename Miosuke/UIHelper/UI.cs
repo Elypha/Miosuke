59d93f374cabf11de3680254004fb1d9c4e42f43
Elypha Rin <i@elypha.com>
2024-10-11 08:31:32 +0900

    add colours

--- Miosuke/UIHelper/UI.cs
+++ Miosuke/UIHelper/UI.cs
@@ -9,59 +9,61 @@ public static class Ui
 {
     // COLOUR
 
-    public static Vector4 ColourAccentDark = HslaToDecimal(25, 0.45, 0.25);
-    public static Vector4 ColourAccentLight = HslaToDecimal(35, 0.75, 0.85);
-    public static Vector4 ColourAccentLightAlt = HslaToDecimal(25, 0.75, 0.85);
-    public static Vector4 ColourBackground = HslaToDecimal(35, 0.8, 0.82);
-    public static Vector4 ColourBackground1 = HslaToDecimal(35, 0.60, 0.77);
-    public static Vector4 ColourBackground1Active = HslaToDecimal(35, 0.60, 0.76);
-    public static Vector4 ColourBackground1Hover = HslaToDecimal(35, 0.60, 0.78);
-    public static Vector4 ColourBackground2 = HslaToDecimal(35, 0.55, 0.72);
-    public static Vector4 ColourBackground3 = HslaToDecimal(35, 0.50, 0.68);
-    public static Vector4 ColourBackground4 = HslaToDecimal(35, 0.45, 0.62);
-    public static Vector4 ColourBackgroundAlt = HslaToDecimal(27, 0.45, 0.70);
-    public static Vector4 ColourForeground = HslaToDecimal(20, 0.35, 0.35);
-    public static Vector4 ColourForegroundComment = HslaToDecimal(25, 0.35, 0.50);
-    public static Vector4 ColourInputHint = HslaToDecimal(35, 0.65, 0.25);
+    public static readonly Vector4 ColourAccentDark = HslaToDecimal(25, 0.45, 0.25);
+    public static readonly Vector4 ColourAccentLight = HslaToDecimal(35, 0.75, 0.85);
+    public static readonly Vector4 ColourAccentLightAlt = HslaToDecimal(25, 0.75, 0.85);
+    public static readonly Vector4 ColourBackground = HslaToDecimal(35, 0.8, 0.82);
+    public static readonly Vector4 ColourBackground1 = HslaToDecimal(35, 0.60, 0.77);
+    public static readonly Vector4 ColourBackground1Active = HslaToDecimal(35, 0.60, 0.76);
+    public static readonly Vector4 ColourBackground1Hover = HslaToDecimal(35, 0.60, 0.78);
+    public static readonly Vector4 ColourBackground2 = HslaToDecimal(35, 0.55, 0.72);
+    public static readonly Vector4 ColourBackground3 = HslaToDecimal(35, 0.50, 0.68);
+    public static readonly Vector4 ColourBackground4 = HslaToDecimal(35, 0.45, 0.62);
+    public static readonly Vector4 ColourBackgroundAlt = HslaToDecimal(27, 0.45, 0.70);
+    public static readonly Vector4 ColourForeground = HslaToDecimal(20, 0.35, 0.35);
+    public static readonly Vector4 ColourForegroundComment = HslaToDecimal(25, 0.35, 0.50);
+    public static readonly Vector4 ColourInputHint = HslaToDecimal(35, 0.65, 0.25);
 
-    public static Vector4 ColourBlack = new(0, 0, 0, 1);
-    public static Vector4 ColourWhite = new(1, 1, 1, 1);
-    public static Vector4 ColourWhiteDim = new(0.95f, 0.95f, 0.95f, 1);
-    public static Vector4 ColourWhite2 = new(1, 1, 1, 0.85f);
-    public static Vector4 ColourWhite3 = new(1, 1, 1, 0.65f);
-    public static Vector4 ColourWhite4 = new(1, 1, 1, 0.50f);
-    public static Vector4 ColourCyan = HslaToDecimal(175, 0.5, 0.75);
-    public static Vector4 ColourCyanDark = HslaToDecimal(190, 0.50, 0.35);
-    public static Vector4 ColourBlue = HslaToDecimal(200, 0.85, 0.6);
-    public static Vector4 ColourSkyBlue = HslaToDecimal(175, 0.25, 0.66);
-    public static Vector4 ColourSkyBlueActive = HslaToDecimal(175, 0.25, 0.64);
-    public static Vector4 ColourSkyBlueHover = HslaToDecimal(175, 0.25, 0.68);
-    public static Vector4 ColourCrimson = HslaToDecimal(5, 0.75, 0.6);
-    public static Vector4 ColourKhaki = HslaToDecimal(25, 0.65, 0.75);
+    public static readonly Vector4 ColourBlack = new(0, 0, 0, 1);
+    public static readonly Vector4 ColourWhite = new(1, 1, 1, 1);
+    public static readonly Vector4 ColourWhiteDim = new(0.95f, 0.95f, 0.95f, 1);
+    public static readonly Vector4 ColourWhite2 = new(1, 1, 1, 0.85f);
+    public static readonly Vector4 ColourWhite3 = new(1, 1, 1, 0.65f);
+    public static readonly Vector4 ColourWhite4 = new(1, 1, 1, 0.50f);
+    public static readonly Vector4 ColourCyan = HslaToDecimal(175, 0.5, 0.75);
+    public static readonly Vector4 ColourCyanDark = HslaToDecimal(190, 0.50, 0.35);
+    public static readonly Vector4 ColourBlue = HslaToDecimal(200, 0.85, 0.6);
+    public static readonly Vector4 ColourSkyBlue = HslaToDecimal(175, 0.25, 0.66);
+    public static readonly Vector4 ColourSkyBlueActive = HslaToDecimal(175, 0.25, 0.64);
+    public static readonly Vector4 ColourSkyBlueHover = HslaToDecimal(175, 0.25, 0.68);
+    public static readonly Vector4 ColourCrimson = HslaToDecimal(5, 0.75, 0.6);
+    public static readonly Vector4 ColourKhaki = HslaToDecimal(25, 0.65, 0.75);
+    public static readonly Vector4 ColourPink = HslaToDecimal(0, 1, 0.85f);
+    public static readonly Vector4 ColourYellow = HslaToDecimal(50, 1, 0.90f);
 
 
-    public static Vector4 ColourHq = HslaToDecimal(45, 0.80, 0.70);
+    public static readonly Vector4 ColourHq = HslaToDecimal(45, 0.80, 0.70);
 
 
-    public static Vector4 Alpha = new(1, 1, 1, 0.55f);
-    public static Vector4 Alpha2 = new(1, 1, 1, 0.45f);
-    public static Vector4 TransparentBackground = new(1, 1, 1, 0.95f);
-    public static Vector4 TransparentBackground2 = new(1, 1, 1, 0.85f);
-    public static Vector4 TransparentBackground3 = new(1, 1, 1, 0.80f);
-    public static Vector4 TransparentBackground4 = new(1, 1, 1, 0.70f);
-    public static Vector4 Transparent = new(1, 1, 1, 0);
+    public static readonly Vector4 Alpha = new(1, 1, 1, 0.55f);
+    public static readonly Vector4 Alpha2 = new(1, 1, 1, 0.45f);
+    public static readonly Vector4 TransparentBackground = new(1, 1, 1, 0.95f);
+    public static readonly Vector4 TransparentBackground2 = new(1, 1, 1, 0.85f);
+    public static readonly Vector4 TransparentBackground3 = new(1, 1, 1, 0.80f);
+    public static readonly Vector4 TransparentBackground4 = new(1, 1, 1, 0.70f);
+    public static readonly Vector4 Transparent = new(1, 1, 1, 0);
 
 
     // STYLE
 
-    public static Vector2 FramePadding = new(8f, 8f);
-    public static Vector2 ItemSpacing = new(6f, 6f);
-    public static Vector2 WindowPadding = new(5f, 5f);
-    public static float FrameRounding = 10.0f;
-    public static float FrameBorderSize = 3.0f;
-    public static float ScrollbarSize = 7.0f;
-    public static float WindowRounding = 10.0f;
-    public static float WindowBorderSize = 3.0f;
+    public static readonly Vector2 FramePadding = new(8f, 8f);
+    public static readonly Vector2 ItemSpacing = new(6f, 6f);
+    public static readonly Vector2 WindowPadding = new(5f, 5f);
+    public static readonly float FrameRounding = 10.0f;
+    public static readonly float FrameBorderSize = 3.0f;
+    public static readonly float ScrollbarSize = 7.0f;
+    public static readonly float WindowRounding = 10.0f;
+    public static readonly float WindowBorderSize = 3.0f;
     public static (ImRaii.Style, ImRaii.Color) PushStyleCollection()
     {
         var style = ImRaii.PushStyle(ImGuiStyleVar.FramePadding, FramePadding)
